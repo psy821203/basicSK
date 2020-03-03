2f7952722a450ad38134084ee133b63f1d3594fd
psy821203 <82sypark@naver.com>
2020-03-03 22:32:26 +0900

    관리자-공지사항관리-패키지 예시 추가

---
+++ src/main/java/com/skmagic/admin/bbs/notice/service/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @패키지명 : com.skmagic.admin.bbs.notice.service
+ * @클래스명 : package-info.class
+ * @작성자 : SYPark
+ * @작성일 : 2020. 3. 3.
+ * @설명 :
+ * @ 소스 변경 내역
+ * @  날짜            수정자        수정내용
+ * @ -------------------------------------------------
+ * @ 2020. 3. 3.   SYPark   최초생성
+ */
+
+package com.skmagic.admin.bbs.notice.service;
